a4aa275e78f25603044705d15781f2de8a3bdee6
André Jesus <andre.jesus.pilar@gmail.com>
2023-12-03 12:27:38 +0000

    feat: dimensionality and granularity studies for covid time series implemented.

--- forecasting/data_profiling/covid/dimensionality.py
+++ forecasting/data_profiling/covid/dimensionality.py
@@ -1,1 +1,42 @@
-# TODO - implement
+import matplotlib.pyplot as plt
+from pandas import DataFrame, read_csv, Series
+
+from utils.dslabs_functions import plot_line_chart, HEIGHT, plot_ts_multivariate_chart
+
+covid_filename: str = "../../data/covid/forecast_covid.csv"
+covid_file_tag: str = "covid"
+covid_data: DataFrame = read_csv(covid_filename, index_col="date", parse_dates=True, infer_datetime_format=True)
+target: str = "deaths"
+
+series: Series = covid_data[target]
+print("Nr. Records = ", series.shape[0])
+print("First timestamp", series.index[0])
+print("Last timestamp", series.index[-1])
+
+# Plot the data at the most atomic granularity - daily (not weekly - that will be studied in the granularity section)
+
+# ------------------
+# Daily Deaths
+# ------------------
+
+plt.figure(figsize=(3 * HEIGHT, HEIGHT / 2))
+plot_line_chart(
+    series.index.to_list(),
+    series.to_list(),
+    xlabel=series.index.name,
+    ylabel=target,
+    title=f"{covid_file_tag} daily {target}",
+)
+plt.tight_layout()
+plt.savefig(f"images/dimensionality/{covid_file_tag}_daily_{target}.png")
+plt.show()
+plt.clf()
+
+# ------------------
+# Multivariate Time Series - All Variables # TODO: Maybe remove the week chart
+# ------------------
+
+plot_ts_multivariate_chart(covid_data, title=f"{covid_file_tag} {target}")
+plt.tight_layout()
+plt.savefig(f"images/dimensionality/{covid_file_tag}_{target}.png")
+plt.show()
--- forecasting/data_profiling/covid/granularity.py
+++ forecasting/data_profiling/covid/granularity.py
@@ -1,1 +1,33 @@
-# TODO - implement
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from pandas import DataFrame, read_csv, Series
+
+from utils.dslabs_functions import plot_line_chart, HEIGHT, plot_ts_multivariate_chart, ts_aggregation_by
+
+covid_filename: str = "../../data/covid/forecast_covid.csv"
+covid_file_tag: str = "covid"
+covid_data: DataFrame = read_csv(covid_filename, index_col="date", parse_dates=True, infer_datetime_format=True)
+target: str = "deaths"
+series: Series = covid_data[target]
+
+grans: list[str] = ["D", "W", "M", "Q"]
+fig: Figure
+axs: list[Axes]
+fig, axs = plt.subplots(len(grans), 1, figsize=(3 * HEIGHT, HEIGHT / 2 * len(grans)))
+fig.suptitle(f"{covid_file_tag} {target} aggregation study")
+
+for i in range(len(grans)):
+    ss: Series = ts_aggregation_by(series, grans[i])
+    plot_line_chart(
+        ss.index.to_list(),
+        ss.to_list(),
+        ax=axs[i],
+        xlabel=f"{ss.index.name} ({grans[i]})",
+        ylabel=target,
+        title=f"granularity={grans[i]}",
+    )
+plt.tight_layout()
+plt.savefig(f"images/granularity/{covid_file_tag}_{target}_aggregation_study.png")
+plt.show()
+plt.clf()
